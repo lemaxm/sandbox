a8e94a948830dd0757f9982f52f3f0b5d2316726
Steve Boardwell <steve.boardwell@datameer.com>
2016-06-27 15:44:16 +0200

    Update git-checkout-util.sh

--- git-checkout-util.sh
+++ git-checkout-util.sh
@@ -6,12 +6,12 @@ IFS=$'\n\t'
 if [ -n "$bamboo_build_working_directory" ]; then
 	# pre-checkout cleaning...
 	echo "Cleaning bamboo_build_working_directory '$bamboo_build_working_directory'..."
-	echo rm -rf "$bamboo_build_working_directory/*"
+	rm -rf "$bamboo_build_working_directory/*"
 	# checkout ad branch handling...
 	cd "$bamboo_build_working_directory"
 	echo "Checking out '$bamboo_planRepository_branchName' with rev '$bamboo_planRepository_revision'"
-	echo git clone $bamboo_planRepository_repositoryUrl .
-	echo git checkout $bamboo_planRepository_branchName
-	echo git checkout $bamboo_planRepository_revision
+	git clone $bamboo_planRepository_repositoryUrl .
+	git checkout $bamboo_planRepository_branchName
+	git checkout $bamboo_planRepository_revision
 	cd -
 fi
